7b3d34a28c9aa6dabc9e19aeef9e9a08426b1002
Waga Odongo <wagaodongo@gmail.com>
2019-01-27 21:49:14 +0300

    product population
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- server/server.js
+++ server/server.js
@@ -35,6 +35,15 @@ const {admin} = require('./middleware/admin');
 
 app.post('/api/product/article', auth, admin, (req,res)=>{
 
+    const product = new Product(req.body);
+
+    product.save((err,doc)=> {
+        if(err) return res.json({success:false,err});
+        res.status(200).json({
+            success:true, 
+            article:doc
+        })
+    })
 
 })
 
