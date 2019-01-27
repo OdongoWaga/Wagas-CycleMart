6cc735e945cdc017084a91e196ca146822f7179a
Waga Odongo <wagaodongo@gmail.com>
2019-01-27 17:06:36 +0300

    bike brands
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,22 +1,21 @@
-const {User} = require('./../models/user');
+const { User } = require('./../models/user');
 
-let auth = (req, res, next) => {
+let auth = (req,res,next) => {
     let token = req.cookies.w_auth;
 
-    User.findByToken(token, (err,user)=> {
-
+    User.findByToken(token,(err,user)=>{
         if(err) throw err;
         if(!user) return res.json({
-            isAuth:false,
-            error:true
+            isAuth: false,
+            error: true
         });
 
-        req.token= token ;
-        req.user= user;
+        req.token = token;
+        req.user = user;
         next();
     })
 
-
 }
 
-module.exports = {auth};
+
+module.exports = { auth }
--- server/models/user.js
+++ server/models/user.js
@@ -1,108 +1,98 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const config = require('../config/config').get(process.env.NODE_ENV);
+const SALT_I = 10;
+require('dotenv').config();
 
-
-const SALT_I=10
-
-
- const userSchema = mongoose.Schema({
-    email: {
+const userSchema = mongoose.Schema({
+    email:{
         type:String,
         required: true,
         trim: true,
         unique: 1
     },
-    password: {
+    password:{
         type:String,
         required: true,
         minlength: 5
     },
-    name: {
+    name:{
         type:String,
         required: true,
-        maxlength: 255
-    }, 
-    lastname: {
+        maxlength:100
+    },
+    lastname:{
         type:String,
         required: true,
-        maxlength: 255
-    },  
-   cart:{
-        type:Array,
-        deafult:[]
+        maxlength:100
     },
-   history:{
+    cart:{
         type:Array,
-        deafult:[]
+        default: []
     },
-   role:{
+    history:{
+        type:Array,
+        default: []
+    },
+    role:{
         type:Number,
-        deafult:0
+        default:0
     },
-    token: {
+    token:{
         type:String
     }
+});
 
- });
+userSchema.pre('save',function(next){
+    var user = this;
 
- userSchema.pre('save', function(next) {
-     var user= this;
-
-     if(user.isModified('password')){
-        bcrypt.genSalt(SALT_I, function(err,salt){
+    if(user.isModified('password')){
+        bcrypt.genSalt(SALT_I,function(err,salt){
             if(err) return next(err);
-   
-            bcrypt.hash(user.password, salt, function(err, hash){
+    
+            bcrypt.hash(user.password,salt,function(err,hash){
                 if(err) return next(err);
-                user.password =hash;
+                user.password = hash;
                 next();
-            }); 
-   
+            });
         })
-     } else{
-         next()
-     }
+    } else{
+        next()
+    }
+})
 
-     
- })
-
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
- bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
-     if(err) return cb(err);
-     cb(null, isMatch)
- })
-
+userSchema.methods.comparePassword = function(candidatePassword,cb){
+    bcrypt.compare(candidatePassword,this.password,function(err,isMatch){
+        if(err) return cb(err);
+        cb(null,isMatch)
+    })
 }
+
 
 userSchema.methods.generateToken = function(cb){
-
     var user = this;
-    var token = jwt.sign(user._id.toHexString(),config.SECRET)
+    var token = jwt.sign(user._id.toHexString(),process.env.SECRET)
 
     user.token = token;
-    user.save( function(err, user){
+    user.save(function(err,user){
         if(err) return cb(err);
-        cb(null, user);
+        cb(null,user);
     })
-
 }
 
-userSchema.statics.findByToken = function(token, cb) {
+userSchema.statics.findByToken = function(token,cb){
     var user = this;
 
-    jwt.verify(token,config.SECRET, function(err, decode) {
-        user.findOne({"_id":decode, "token": token }, function(err,user){
+    jwt.verify(token,process.env.SECRET,function(err,decode){
+        user.findOne({"_id":decode,"token":token},function(err,user){
             if(err) return cb(err);
-            cb(null, user)
+            cb(null,user);
         })
     })
 }
 
 
 
+const User = mongoose.model('User',userSchema);
 
- const User = mongoose.model('User', userSchema);
-
- module.exports ={User}
+module.exports = { User }
