5534cf8178e3fabed8ba226009b4b9054a2c9a1f
Waga Odongo <wagaodongo@gmail.com>
2019-01-28 21:02:01 +0300

    Header
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/components/header_footer/Header/index.js
+++ client/src/components/header_footer/Header/index.js
@@ -1,6 +1,117 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link, withRouter} from 'react-router-dom';
+
+import { connect } from 'react-redux';
+
 
  class Header extends Component {
+
+  state = {
+    page:[
+        {
+            name:'Home',
+            linkTo:'/',
+            public: true
+        },
+        {
+            name:'Bikes',
+            linkTo:'/shop',
+            public: true
+        }
+    ],
+    user:[
+        {
+            name:'My Cart',
+            linkTo:'/user/cart',
+            public: false
+        },
+        {
+            name:'My Account',
+            linkTo:'/user/dashboard',
+            public: false
+        },
+        {
+            name:'Log in',
+            linkTo:'/register_login',
+            public: true
+        },
+        {
+            name:'Log out',
+            linkTo:'/user/logout',
+            public: false
+        },
+    ]
+}
+
+
+logoutHandler = () => {
+  this.props.dispatch(logoutUser()).then(response =>{
+      if(response.payload.success){
+          this.props.history.push('/')
+      }
+  })
+}
+
+
+cartLink = (item,i) => {
+  const user = this.props.user.userData;
+
+  return (
+      <div className="cart_link" key={i}>
+          <span>{user.cart ? user.cart.length:0}</span>
+          <Link to={item.linkTo}>
+              {item.name}
+          </Link>
+      </div>
+  )
+}
+
+
+defaultLink = (item,i) => (
+  item.name === 'Log out' ?
+      <div className="log_out_link"
+          key={i}
+          onClick={()=> this.logoutHandler()}
+      >
+          {item.name}
+      </div>
+
+  :
+  <Link to={item.linkTo} key={i}>
+      {item.name}
+  </Link>
+)
+
+
+showLinks = (type) =>{
+  let list = [];
+
+  if(this.props.user.userData){
+      type.forEach((item)=>{
+          if(!this.props.user.userData.isAuth){
+              if(item.public === true){
+                  list.push(item)
+              }
+          } else{
+              if(item.name !== 'Log in'){
+                  list.push(item)
+              }
+          }
+      });
+  }
+
+  return list.map((item,i)=>{
+      if(item.name !== 'My Cart'){
+          return this.defaultLink(item,i)
+      } else {
+          return this.cartLink(item,i)
+      }
+      
+  })
+}
+
+
+
   render() {
     return (
       <header className ="bck_b_light">
@@ -14,10 +125,10 @@ import React, { Component } from 'react'
 
       <div className="right">
       <div className="top">
-      LINKS
+      {this.showLinks(this.state.user)}
       </div>
       <div className ="bottom">
-      LINKS
+      {this.showLinks(this.state.page)}
       </div>
       </div>
       
@@ -30,4 +141,12 @@ import React, { Component } from 'react'
   }
 }
 
-export default Header
+
+
+function mapStateToProps(state){
+  return {
+      user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(withRouter(Header));
