a7d2a1d5fda9d8538592c6c9d5be1c169c792bea
Waga Odongo <wagaodongo@gmail.com>
2019-01-29 10:24:42 +0300

    product reducer
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react'
 import HomeSlider from './home_slider';
 import HomePromotion from './home_promotion';
 
+import { connect } from 'react-redux';
+import { getProductsBySale, getProductsByArrival } from '../../actions/product_actions';
+
+
  class Home extends Component {
+
+  componentDidMount(){
+    this.props.dispatch(getProductsBySale());
+    this.props.dispatch(getProductsByArrival());
+}
+
+
   render() {
     return (
       <div>
@@ -13,4 +24,10 @@ import HomePromotion from './home_promotion';
   }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+  return {
+      products: state.products
+  }
+}
+
+export default connect(mapStateToProps)(Home);
