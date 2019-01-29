fed4d171a4649027560507154e1c3be6c10fa64a
Waga Odongo <wagaodongo@gmail.com>
2019-01-29 17:21:29 +0300

    routes admin
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/routes.js
+++ client/src/routes.js
@@ -5,7 +5,8 @@ import Layout from './hoc/layout';
 import RegisterLogin from './components/authorisation';
 import Register from './components/authorisation/register';
 import Auth from './hoc/auth';
-import Shop from './components/Shop'
+import Shop from './components/Shop';
+import AddProduct from './components/User/Admin/add_product';
 
 import UserDashboard from './components/User'
 
@@ -17,6 +18,7 @@ const Routes = () => {
       <Switch>
       <Route path="/user/dashboard" exact component={Auth(UserDashboard,true)}/>
 
+      <Route path="/admin/add_product" exact component={Auth(AddProduct,true)}/>
         <Route path="/register" exact component={Auth(Register,false)}/>
         <Route path="/register_login" exact component={Auth(RegisterLogin,false)}/>
         <Route path="/" exact component={Auth(Home,null)}/>
