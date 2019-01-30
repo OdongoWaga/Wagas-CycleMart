9ac024f87ece09a89d92e34477d5686beb741677
Waga Odongo <wagaodongo@gmail.com>
2019-01-30 18:09:11 +0300

    routes
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/routes.js
+++ client/src/routes.js
@@ -13,6 +13,8 @@ import ManageCategories from './components/User/Admin/manage_categories';
 import UserDashboard from './components/User';
 import UserCart from './components/User/cart';
 import UpdateProfile from './components/User/update_profile';
+import ManageSite from './components/User/Admin/manage_site';
+import AddFile from './components/User/Admin/add_file';
 
 
 const Routes = () => {
@@ -23,6 +25,8 @@ const Routes = () => {
       <Route path="/user/dashboard" exact component={Auth(UserDashboard,true)}/>
       <Route path="/admin/manage_categories" exact component={Auth(ManageCategories,true)}/>
       <Route path="/user/user_profile" exact component={Auth(UpdateProfile,true)}/>
+      <Route path="/admin/site_info" exact component={Auth(ManageSite,true)}/>
+        <Route path="/admin/add_file" exact component={Auth(AddFile,true)}/>
 
       <Route path="/user/cart" exact component={Auth(UserCart,true)}/>
       <Route path="/admin/add_product" exact component={Auth(AddProduct,true)}/>
